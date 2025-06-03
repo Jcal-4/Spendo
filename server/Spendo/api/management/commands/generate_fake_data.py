6c8c9abe1b61001a12ed95bee41831745c9ca075
Jairo <jairo_cal@yahoo.com>
2025-06-02 22:48:51 -0700

    created mock data for transactions and transaction_types

--- server/Spendo/api/management/commands/generate_fake_data.py
+++ server/Spendo/api/management/commands/generate_fake_data.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from api.models import CustomUser, Income, Account
+from api.models import CustomUser, Income, Account, Transaction, TransactionType
 from django.utils import timezone
 import random
 
@@ -11,15 +11,28 @@ class Command(BaseCommand):
         parser.add_argument('--users', type=int, default=10, help='Number of users to create')
         parser.add_argument('--incomes', type=int, default=5, help='Number of incomes per user')
         parser.add_argument('--accounts', type=int, default=2, help='Number of accounts per user')
+        parser.add_argument('--user_transactions', type=int, default=3, help='Number of transactions per user')
 
     # Handle method is the first to initiate when the file is called
     def handle(self, *args, **options):          
         users_count = options['users']
         incomes_per_user = options['incomes']
         accounts_per_user = options['accounts']
+        transaction_per_user = options['user_transactions']
         occupations = ['Engineer', 'Teacher', 'Doctor', 'Artist', 'Developer', 'Designer']
         income_types = ['Salary', 'Bonus', 'Freelance', 'Investment', 'Gift']
         institutions = ['Checking', 'Savings', "Investment", '401k', 'Trusts', 'Credit']
+        user_transactions = ['ubereats', 'postmates', 'internet', 'att', 'haircut', 'groceries', 'steam game']
+
+        # Create transaction types
+        transaction_types = ['scheduled', 'one-time']
+        transaction_type_records = list()
+        for transaction in transaction_types:
+            transaction = TransactionType.objects.create(
+                type = transaction
+            )
+            transaction_type_records.append(transaction)
+            self.stdout.write(self.style.SUCCESS(f'Created Transaction Type: {transaction.type}'))
 
         # Create a User record (varrying on the amount requested or default)
         for i in range(users_count):
@@ -52,6 +65,15 @@ class Command(BaseCommand):
                 )
                 self.stdout.write(self.style.SUCCESS(f' Added Account: {account.institution}'))
                 
-            
+            # For each User being created, create a related Transaction record
+            for j in range(transaction_per_user):
+                transaction = Transaction.objects.create(
+                    name = random.choice(user_transactions),
+                    payment = round(random.uniform(1,100), 2),
+                    recurring = random.choice([True, False]),
+                    user = custom_user,
+                    transactiontype = random.choice(transaction_type_records)
+                )
+                self.stdout.write(self.style.SUCCESS(f' Added User Transaction: {transaction.name}'))
 
         self.stdout.write(self.style.SUCCESS('Fake data generation complete.'))
