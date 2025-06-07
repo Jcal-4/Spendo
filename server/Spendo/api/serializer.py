0035e6e29e328c4d44d894ae6559c71d51492597
Jairo Calderon <jairo_cal@yahoo.com>
2025-06-07 03:07:57 -0700

    serialized all data for models

--- server/Spendo/api/serializer.py
+++ server/Spendo/api/serializer.py
@@ -1,8 +1,32 @@
 from rest_framework import serializers
-from .models import CustomUser
+from .models import CustomUser, IncomeType, Income, Transaction, TransactionType, Account
 
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = '__all__'
         
+class IncomeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Income
+        fields = '__all__'
+        
+class IncomeTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = IncomeType
+        fields = '__all__'
+
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = '__all__'
+        
+class TransactionTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TransactionType
+        fields = '__all__'
+        
+class AccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = '__all__'
