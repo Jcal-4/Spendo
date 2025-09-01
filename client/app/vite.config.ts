9364287db32611965660c962decc20e8e04c5295
Jairo Calderon <jairo_cal@yahoo.com>
2025-08-31 20:38:24 -0700

    updated to fix local execution

--- client/app/vite.config.ts
+++ client/app/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react(), tailwindcss()],
-  base: '/static/', // This ensures assets are loaded from /static/
-});
+  base: mode === 'production' ? '/static/' : '/',
+}));
